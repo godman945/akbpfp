3e028b8a526ac0353c904604a569a1e4905ac634
alex <alex@alex4653.online.com.tw>
2015-06-10 10:11:02 +0800

    update

--- WebContent/WEB-INF/src/com/pchome/akbpfp/db/service/report/AdReportService.java
+++ WebContent/WEB-INF/src/com/pchome/akbpfp/db/service/report/AdReportService.java
@@ -30,8 +30,8 @@ public class AdReportService implements IAdReportService {
 			for (int i=0; i<dataList.size(); i++) {
 				AdReportVO adReportVO = dataList.get(i);
 
-				//String htmlCode = "<iframe height=\"120\" width=\"350\" src=\"adModel.html?adNo=" + adReportVO.getAdSeq() + "&tproNo=" + adReportVO.getTemplateProductSeq() + "\" marginwidth=\"0\" marginheight=\"0\" scrolling=\"no\" frameborder=\"0\" align=\"ceneter\" class=\"akb_iframe\"></iframe>";
-				String htmlCode = "<iframe height=\"120\" width=\"350\" src=\"adModel.html?adNo=" + adReportVO.getAdSeq() + "&tproNo=tpro_201306280001\" marginwidth=\"0\" marginheight=\"0\" scrolling=\"no\" frameborder=\"0\" align=\"ceneter\" class=\"akb_iframe\"></iframe>";
+				String htmlCode = "<iframe height=\"120\" width=\"350\" src=\"adModel.html?adNo=" + adReportVO.getAdSeq() + "&tproNo=" + adReportVO.getTemplateProductSeq() + "\" marginwidth=\"0\" marginheight=\"0\" scrolling=\"no\" frameborder=\"0\" align=\"ceneter\" class=\"akb_iframe\"></iframe>";
+//				String htmlCode = "<iframe height=\"120\" width=\"350\" src=\"adModel.html?adNo=" + adReportVO.getAdSeq() + "&tproNo=tpro_201306280001\" marginwidth=\"0\" marginheight=\"0\" scrolling=\"no\" frameborder=\"0\" align=\"ceneter\" class=\"akb_iframe\"></iframe>";
 
 				adReportVO.setAdPreview(htmlCode);
 
