4a68665b1d50c7c9d4fea1b5bb2b7a42c438d5ec
alex <alex@alex4653.online.com.tw>
2015-06-10 13:26:08 +0800

    update

--- WebContent/WEB-INF/src/com/pchome/akbpfp/struts2/action/ad/AdEditAction.java
+++ WebContent/WEB-INF/src/com/pchome/akbpfp/struts2/action/ad/AdEditAction.java
@@ -166,10 +166,6 @@ public class AdEditAction extends BaseCookieAction{
 		// 上傳圖片暫存檔名(亂數產生)
 		ulTmpName = RandomStringUtils.randomAlphanumeric(30);
 
-		response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
-		response.setHeader("Pragma", "no-cache");
-		response.setDateHeader("Expires", 0);
-		
 		return SUCCESS;
 	}
 
