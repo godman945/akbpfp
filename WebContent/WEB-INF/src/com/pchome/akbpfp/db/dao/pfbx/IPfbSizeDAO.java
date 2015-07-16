f0d99445238c763f87b9f5ab9ab0abc039f264c9
alex <alex@alex4653.online.com.tw>
2015-07-16 10:17:16 +0800

    update

---
+++ WebContent/WEB-INF/src/com/pchome/akbpfp/db/dao/pfbx/IPfbSizeDAO.java
@@ -0,0 +1,7 @@
+package com.pchome.akbpfp.db.dao.pfbx;
+
+import com.pchome.akbpfp.db.dao.IBaseDAO;
+import com.pchome.akbpfp.db.pojo.PfbxSize;
+
+public interface IPfbSizeDAO extends IBaseDAO<PfbxSize, String> {
+}
