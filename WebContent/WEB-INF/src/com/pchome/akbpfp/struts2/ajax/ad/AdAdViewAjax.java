3a727970e73e9c859bf6e71894fa389eacdbe2fc
timchen <timchen@staff.pchome.com.tw>
2015-08-18 17:39:39 +0800

    reback imgSize by tim

--- WebContent/WEB-INF/src/com/pchome/akbpfp/struts2/ajax/ad/AdAdViewAjax.java
+++ WebContent/WEB-INF/src/com/pchome/akbpfp/struts2/ajax/ad/AdAdViewAjax.java
@@ -79,9 +79,9 @@ public class AdAdViewAjax extends BaseCookieAction{
 				totalSize = adAdViewVO.size();		
 				for(PfpAdAdViewVO vo:adAdViewVO){
 					Map<String,String> imgmap = new HashMap<String,String>();
-					imgmap = getImgSize(vo.getOriginalImg());
-					vo.setImgWidth(imgmap.get("imgWidth"));
-					vo.setImgHeight(imgmap.get("imgHeight"));
+					//imgmap = getImgSize(vo.getOriginalImg());
+					//vo.setImgWidth(imgmap.get("imgWidth"));
+					//vo.setImgHeight(imgmap.get("imgHeight"));
 					
 					totalPv += vo.getAdPv();
 					totalClk += vo.getAdClk();		
@@ -107,27 +107,33 @@ public class AdAdViewAjax extends BaseCookieAction{
 	
 	public Map<String,String> getImgSize(String originalImg){
 		Map<String,String> imgmap = new HashMap<String,String>();
+		//File picture = new File(request.getContextPath() + "\\" +  originalImg.replace("/", "\\"));
 		String imgWidth = "0";
 		String imgHeight = "0";
-		
-		InputStream urlimg = null;
-		BufferedImage sourceImg = null;
-		try {
-			URL input = new URL(request.getScheme()+"://"+request.getServerName() + request.getContextPath() + "/" + 
+		log.info("------------------1.originalImg=" + request.getContextPath() + "\\" + originalImg.replace("/", "\\"));
+		log.info("------------------2.url=" + request.getScheme()+"://"+request.getServerName() + request.getContextPath() + "/" + 
 					originalImg.replace("\\", "/") + "?customerInfoId=" + super.getCustomer_info_id());
-			HttpURLConnection conn = (HttpURLConnection)input.openConnection();
-			conn.setRequestMethod("GET");
-			urlimg = conn.getInputStream();
-			sourceImg = javax.imageio.ImageIO.read(urlimg);
-			imgWidth = Integer.toString(sourceImg.getWidth());
-			imgHeight = Integer.toString(sourceImg.getHeight());
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		//if(picture != null){
+			FileInputStream is = null;
+			InputStream urlimg = null;
+			BufferedImage sourceImg = null;
+			try {
+				URL input = new URL("http://showstg.pchome.com.tw/pfp/img/user/AC2013071700001/20150814/original/ad_201508140041.jpg?customerInfoId=AC2013071700001");
+				HttpURLConnection conn = (HttpURLConnection)input.openConnection();
+				conn.setRequestMethod("GET");
+				urlimg = conn.getInputStream();
+				//is = new FileInputStream(picture);
+				sourceImg = javax.imageio.ImageIO.read(urlimg);
+				imgWidth = Integer.toString(sourceImg.getWidth());
+				imgHeight = Integer.toString(sourceImg.getHeight());
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		//}
 		imgmap.put("imgWidth", imgWidth);
 		imgmap.put("imgHeight", imgHeight);
 		
