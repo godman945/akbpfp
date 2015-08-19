60f3e9fe92c86ea7c78f1f150fc2eee3b1421dfe
timchen <timchen@staff.pchome.com.tw>
2015-08-19 10:14:38 +0800

    imgSize test by tim

--- WebContent/WEB-INF/src/com/pchome/akbpfp/struts2/ajax/ad/AdAdViewAjax.java
+++ WebContent/WEB-INF/src/com/pchome/akbpfp/struts2/ajax/ad/AdAdViewAjax.java
@@ -107,8 +107,8 @@ public class AdAdViewAjax extends BaseCookieAction{
 	
 	public Map<String,String> getImgSize(String originalImg){
 		Map<String,String> imgmap = new HashMap<String,String>();
-		File testFile = new File("\\home\\webuser\\akb\\pfp\\");
-		File picture = new File(testFile.getAbsoluteFile() +  originalImg.replace("/", "\\"));
+		File testFile = new File("/home/webuser/akb/pfp/");
+		File picture = new File(testFile.getAbsoluteFile() +  originalImg.replace("\\", "/"));
 		String imgWidth = "0";
 		String imgHeight = "0";
 		log.info("------------------1.originalImg=" + request.getContextPath() + "\\" + originalImg.replace("/", "\\"));
