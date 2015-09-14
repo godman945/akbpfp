1605d0c9896606d5173a5f747eb368dae2e1723d
timchen <timchen@staff.pchome.com.tw>
2015-09-14 16:42:50 +0800

    apply update by tim

---
+++ WebContent/WEB-INF/src/com/pchome/akbpfd/db/vo/user/PfdUserAdAccountRefVO.java
@@ -0,0 +1,50 @@
+package com.pchome.akbpfd.db.vo.user;
+
+
+
+public class PfdUserAdAccountRefVO {
+	
+	private Integer refId;
+	private String pfdCustomerInfoId;
+	private String pfdUserId;
+	private String pfpCustomerInfoId;
+	private String pfpUserId;
+	private String pfpPayType;
+	public Integer getRefId() {
+		return refId;
+	}
+	public void setRefId(Integer refId) {
+		this.refId = refId;
+	}
+	public String getPfdCustomerInfoId() {
+		return pfdCustomerInfoId;
+	}
+	public void setPfdCustomerInfoId(String pfdCustomerInfoId) {
+		this.pfdCustomerInfoId = pfdCustomerInfoId;
+	}
+	public String getPfdUserId() {
+		return pfdUserId;
+	}
+	public void setPfdUserId(String pfdUserId) {
+		this.pfdUserId = pfdUserId;
+	}
+	public String getPfpCustomerInfoId() {
+		return pfpCustomerInfoId;
+	}
+	public void setPfpCustomerInfoId(String pfpCustomerInfoId) {
+		this.pfpCustomerInfoId = pfpCustomerInfoId;
+	}
+	public String getPfpUserId() {
+		return pfpUserId;
+	}
+	public void setPfpUserId(String pfpUserId) {
+		this.pfpUserId = pfpUserId;
+	}
+	public String getPfpPayType() {
+		return pfpPayType;
+	}
+	public void setPfpPayType(String pfpPayType) {
+		this.pfpPayType = pfpPayType;
+	}
+	
+}
