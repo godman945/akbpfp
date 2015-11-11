b81e57353ba5d4216b7f5b724f79ca27c09f2de5
timchen <timchen@staff.pchome.com.tw>
2015-11-11 14:46:15 +0800

    enumAdChannelPC add 300x100 by tim

--- WebContent/WEB-INF/src/com/pchome/enumerate/ad/EnumAdChannelPCSize.java
+++ WebContent/WEB-INF/src/com/pchome/enumerate/ad/EnumAdChannelPCSize.java
@@ -5,7 +5,8 @@ public enum EnumAdChannelPCSize {
 	adp_201510190002("13", "160", "600"),
 	adp_201507210006("10", "250", "80"),
 	adp_201507210001("1", "300", "250"),
-	adp_201507210005("9", "728", "90");
+	adp_201507210005("9", "728", "90"),
+	adp_201507210004("2", "300", "100");
 	
 	private final String name;
 	private final String widh;
