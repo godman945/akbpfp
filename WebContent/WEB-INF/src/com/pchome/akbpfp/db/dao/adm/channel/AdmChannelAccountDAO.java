34c0fff7e4b275aa029a1daf7bf3a06d04658a12
timchen <timchen@staff.pchome.com.tw>
2015-11-27 11:30:18 +0800

    add AdmChannelAccountDAO by tim

---
+++ WebContent/WEB-INF/src/com/pchome/akbpfp/db/dao/adm/channel/AdmChannelAccountDAO.java
@@ -0,0 +1,8 @@
+package com.pchome.akbpfp.db.dao.adm.channel;
+
+import com.pchome.akbpfp.db.dao.BaseDAO;
+import com.pchome.akbpfp.db.pojo.AdmChannelAccount;
+
+public class AdmChannelAccountDAO  extends BaseDAO <AdmChannelAccount, String> implements IAdmChannelAccountDAO {
+
+}
---
+++ WebContent/WEB-INF/src/com/pchome/akbpfp/db/dao/adm/channel/IAdmChannelAccountDAO.java
@@ -0,0 +1,9 @@
+package com.pchome.akbpfp.db.dao.adm.channel;
+
+import com.pchome.akbpfp.db.dao.IBaseDAO;
+import com.pchome.akbpfp.db.pojo.AdmChannelAccount;
+
+
+public interface IAdmChannelAccountDAO extends IBaseDAO<AdmChannelAccount, String> {
+
+}
