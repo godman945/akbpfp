8d435e0d7bd49c30c88fabc1e6f8ef0b9cedd949
timchen <timchen@staff.pchome.com.tw>
2017-07-04 14:13:50 +0800

    update by tim

--- WebContent/WEB-INF/src/com/pchome/akbpfp/db/service/report/AdReportService.java
+++ WebContent/WEB-INF/src/com/pchome/akbpfp/db/service/report/AdReportService.java
@@ -40,6 +40,9 @@ public class AdReportService implements IAdReportService {
 		    String img = null;
 		    String title = null;
 		    String adStyle = null;
+		    String adSize = null;
+		    String html5Title = null;
+		    String html5Flag = "N";
 
 			for (int i=0; i<dataList.size(); i++) {
 				AdReportVO adReportVO = dataList.get(i);
@@ -49,6 +52,9 @@ public class AdReportService implements IAdReportService {
 	            img = null;
 	            title = null;
 	            adStyle = null;
+	            adSize = null;
+			    html5Title = null;
+			    html5Flag = "N";
 				for (PfpAdDetail pfpAdDetail: pfpAdDetailList) {
                     if ("real_url".equals(pfpAdDetail.getAdDetailId())) {
                         realUrl = pfpAdDetail.getAdDetailContent();
@@ -59,60 +65,87 @@ public class AdReportService implements IAdReportService {
                     if ("title".equals(pfpAdDetail.getAdDetailId())) {
                     	title = pfpAdDetail.getAdDetailContent();
                     }
+                    if("size".equals(pfpAdDetail.getAdDetailId())){
+                    	adSize = pfpAdDetail.getAdDetailContent();
+                    }
+                    if("zip".equals(pfpAdDetail.getAdDetailId())){
+                    	html5Title = pfpAdDetail.getAdDetailContent();
+                    }
                     if (StringUtils.isBlank(adStyle)) {
                         adStyle = pfpAdDetail.getPfpAd().getAdStyle();
                     }
+                    if(StringUtils.equals("c_x05_po_tad_0059", pfpAdDetail.getPfpAd().getAdAssignTadSeq())){
+                    	html5Flag = "Y";
+                    }
 				}
 
-				//String htmlCode = "<iframe height=\"120\" width=\"350\" src=\"adModel.html?adNo=" + adReportVO.getAdSeq() + "&tproNo=" + adReportVO.getTemplateProductSeq() + "\" marginwidth=\"0\" marginheight=\"0\" scrolling=\"no\" frameborder=\"0\" align=\"ceneter\" class=\"akb_iframe\"></iframe>";
 				String htmlCode = "";
 				if ("IMG".equals(adStyle)) {
 					//取得圖片尺寸
 					String imgWidth = "0";
 					String imgHeight = "0";
-					File picture = null;
-					FileInputStream is = null;
-					BufferedImage sourceImg = null;
-					try{
-						picture = new File("/home/webuser/akb/pfp/" +  img.replace("\\", "/"));
-						if(picture != null){
-							is = new FileInputStream(picture);
-							sourceImg = javax.imageio.ImageIO.read(is);
-							imgWidth = Integer.toString(sourceImg.getWidth());
-							imgHeight = Integer.toString(sourceImg.getHeight());	
-						}
-					} catch (FileNotFoundException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} finally {
-						if(is != null){
-							is.close();
-						}
-					}
-					
 					String showUrl = realUrl;
 					showUrl = showUrl.replaceAll("http://", "");
 					showUrl = showUrl.replaceAll("https://", "");
-	            	if(showUrl.lastIndexOf(".com/") != -1){
-	            		showUrl = showUrl.substring(0, showUrl.lastIndexOf(".com/") + 4);
-	            	}
-	            	if(showUrl.lastIndexOf(".tw/") != -1){
-	            		showUrl = showUrl.substring(0, showUrl.lastIndexOf(".tw/") + 3);
-	            	}
-					
-					//組html畫面
-					htmlCode = "<div class=\"adreportdv\">";
-					htmlCode += "<span class=\"adboxdvimg\"><a href=\"" + realUrl + "\" target=\"_blank\"><img src=\"" + img + "\" /></a></span>";
-					htmlCode += "<span class=\"adboxdvinf\"><span>";
-					if(title != null){
-						htmlCode += "<b>" + title + "</b><br>";
+					if(showUrl.lastIndexOf(".com/") != -1){
+						showUrl = showUrl.substring(0, showUrl.lastIndexOf(".com/") + 4);
 					}
-					htmlCode += "<i>尺寸</i><b>" + imgWidth + " x " + imgHeight + "</b><br>";
-					htmlCode += ("<span>" + showUrl + "</span><br><a style=\"cursor:pointer\" onclick=\"preview('" + img + "')\">預覽</a>");
-					htmlCode += "</span></span></div>";
+					if(showUrl.lastIndexOf(".tw/") != -1){
+						showUrl = showUrl.substring(0, showUrl.lastIndexOf(".tw/") + 3);
+					}
+					
+					if(StringUtils.equals("Y", html5Flag)){
+						String[] sizeArray = adSize.split("x");
+						imgWidth = sizeArray[0].trim();
+						imgHeight = sizeArray[1].trim();
+						
+						//組html畫面
+						htmlCode = "<div class=\"adreportdv\">";
+						htmlCode += "<span class=\"adboxdvimg\"><span>" + html5Title + "</span></a></span>";
+						htmlCode += "</span>";
+						htmlCode += "<span class=\"adboxdvinf\"><span>";
+						if(title != null){
+							htmlCode += "<b>" + title + "</b><br>";
+						}
+						htmlCode += "<i>尺寸</i><b>" + imgWidth + " x " + imgHeight + "</b><br>";
+						htmlCode += ("<span>" + showUrl + "</span><br><a style=\"cursor:pointer\" onclick=\"previewHtml5('" + imgWidth + "','" + imgHeight + "','" + img + "')\">預覽</a>");
+						htmlCode += "</span></span></div>";
+					} else {
+						File picture = null;
+						FileInputStream is = null;
+						BufferedImage sourceImg = null;
+						try{
+							picture = new File("/home/webuser/akb/pfp/" +  img.replace("\\", "/"));
+							if(picture != null){
+								is = new FileInputStream(picture);
+								sourceImg = javax.imageio.ImageIO.read(is);
+								imgWidth = Integer.toString(sourceImg.getWidth());
+								imgHeight = Integer.toString(sourceImg.getHeight());	
+							}
+						} catch (FileNotFoundException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} finally {
+							if(is != null){
+								is.close();
+							}
+						}
+						
+						
+						//組html畫面
+						htmlCode = "<div class=\"adreportdv\">";
+						htmlCode += "<span class=\"adboxdvimg\"><a href=\"" + realUrl + "\" target=\"_blank\"><img src=\"" + img + "\" /></a></span>";
+						htmlCode += "<span class=\"adboxdvinf\"><span>";
+						if(title != null){
+							htmlCode += "<b>" + title + "</b><br>";
+						}
+						htmlCode += "<i>尺寸</i><b>" + imgWidth + " x " + imgHeight + "</b><br>";
+						htmlCode += ("<span>" + showUrl + "</span><br><a style=\"cursor:pointer\" onclick=\"preview('" + img + "')\">預覽</a>");
+						htmlCode += "</span></span></div>";
+					}
 					
 					adReportVO.setContent("尺寸：" + imgWidth + " x " + imgHeight);
 				} else {
