ef9f47b3a3db9d0d1c2ff0a63a8b0b2321d5da80
alexchen <alexchen@alexchen6265.online.com.tw>
2017-10-18 16:02:42 +0800

    update

--- WebContent/html/js/ad/adAddVideo.js
+++ WebContent/html/js/ad/adAddVideo.js
@@ -736,7 +736,7 @@ function appendVideoPreview(){
 				iframe = $($(iframe).children()[1]).children()[0];
 				
 				console.log(iframe);
-				console.log($(iframe)[0]);
+				console.log($(iframe)[0].document);
 				
 //				console.log(iframe.contentDocument);
 //				console.log(iframe.contentDocument.getElementsByTagName("head")[0]);
