5419ba41de2419db087cb1e89639fa91e1b5f7c1
alexchen <alexchen@alexchen6265.online.com.tw>
2017-10-18 09:09:17 +0800

    update

--- WebContent/html/js/ad/adAddVideo.js
+++ WebContent/html/js/ad/adAddVideo.js
@@ -64,6 +64,7 @@ $(document).ready(function(){
 	$("#adVideoURL").blur(function() {
 		if($("#adVideoURL").val() == ""){
 			$("#adVideoURLMsg").text('請輸入影片網址');
+			return false;
 		}
 		var regx = new RegExp(/^[hH][tT][tT][pP]([sS]?):\/\/(\S+\.)+\S{2,}$/);
 		if($("#adVideoURL").val() == ""){
@@ -703,7 +704,6 @@ function appendVideoPreview(){
 			});
 			
 			if(!createPreViewVideoExist){
-				var url = adPreviewVideoData.previewUrl;
 				var width = radioObj.name.substring(0,3);
 				var height = radioObj.name.substring(3,radioObj.name.length);
 				var a = 
@@ -713,7 +713,7 @@ function appendVideoPreview(){
 				   '</div>'+
 				   '<div  class="v_preview box_a_style">'+
 				   '<iframe class="akb_iframe"  scrolling="no" frameborder="0" marginwidth="0" marginheight="0" vspace="0" hspace="0" id="pchome8044_ad_frame1" width="'+width+'" height="'+height+'" allowtransparency="true" allowfullscreen="true"' +
-				   'src="adVideoPreview.html?adPreviewVideoURL='+encodeURIComponent(url)+'&adPreviewVideoBgImg='+imgSrc+'"></iframe>';
+				   'src="adVideoPreview.html?adPreviewVideoURL='+encodeURIComponent(imgSrc)+'&adPreviewVideoBgImg='+imgSrc+'"></iframe>';
 				   '</div>'+
 				'</div>';
 				$("#preViewArea").append(a);
