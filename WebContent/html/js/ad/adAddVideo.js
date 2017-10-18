90b7b1139eb9a94792c909640813ccbdec36ebfa
alexchen <alexchen@alexchen6265.online.com.tw>
2017-10-18 09:29:11 +0800

    update

--- WebContent/html/js/ad/adAddVideo.js
+++ WebContent/html/js/ad/adAddVideo.js
@@ -696,7 +696,7 @@ function appendVideoPreview(){
 			var imgSrc = radioObj.parentElement.parentElement.parentElement.getElementsByTagName("img")[0].src;
 			
 			console.log(imgSrc);
-			
+			console.log(adPreviewVideoData);
 			
 			$("#preViewArea input[type=checkbox]").each(function(index,checkboxObj){
 				var size = checkboxObj.id.replace("checkbox_","");
@@ -710,6 +710,7 @@ function appendVideoPreview(){
 				console.log("createPreViewVideoExist");
 				var width = radioObj.name.substring(0,3);
 				var height = radioObj.name.substring(3,radioObj.name.length);
+				var paramater = encodeURIComponent('?adPreviewVideoURL='+url+'&adPreviewVideoBgImg='+imgSrc);
 				var a = 
 				'<div class="v_box">'+
 				   '<div class="">'+
@@ -717,7 +718,7 @@ function appendVideoPreview(){
 				   '</div>'+
 				   '<div  class="v_preview box_a_style">'+
 				   '<iframe class="akb_iframe"  scrolling="no" frameborder="0" marginwidth="0" marginheight="0" vspace="0" hspace="0" id="pchome8044_ad_frame1" width="'+width+'" height="'+height+'" allowtransparency="true" allowfullscreen="true"' +
-				   'src="adVideoPreview.html?adPreviewVideoURL='+encodeURIComponent(url)+'&adPreviewVideoBgImg='+encodeURIComponent(imgSrc)+'"></iframe>';
+				   'src="adVideoPreview.html'+paramater+'"></iframe>';
 				   '</div>'+
 				'</div>';
 				$("#preViewArea").append(a);
