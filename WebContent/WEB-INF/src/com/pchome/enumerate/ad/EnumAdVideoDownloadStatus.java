58a68ba9dad4e71d57c10c1dddafe3746effb30b
alexchen <alexchen@alexchen6265.online.com.tw>
2017-10-19 13:54:40 +0800

    update

---
+++ WebContent/WEB-INF/src/com/pchome/enumerate/ad/EnumAdVideoDownloadStatus.java
@@ -0,0 +1,27 @@
+package com.pchome.enumerate.ad;
+
+public enum EnumAdVideoDownloadStatus {
+	
+	WAIT(0, "尚未下載"),
+	FINISH(1, "下載完成"),
+	DOWNLOAD(2, "下載中"),
+	MP4_FAIL(3, "mp4下載失敗"),
+	WEBM_FAIL(4, "webm下載失敗");
+	
+	private final int status;
+	private final String downloadValue;
+	
+	private EnumAdVideoDownloadStatus(int status, String downloadValue){
+		this.status = status;
+		this.downloadValue = downloadValue;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public String getDownloadValue() {
+		return downloadValue;
+	}
+
+}
