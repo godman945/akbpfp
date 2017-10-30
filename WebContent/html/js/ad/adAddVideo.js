3fbc006201d602316e4711ae475ac1c537b2e986
alexchen <alexchen@alexchen6265.online.com.tw>
2017-10-30 13:22:38 +0800

    update

--- WebContent/html/js/ad/adAddVideo.js
+++ WebContent/html/js/ad/adAddVideo.js
@@ -733,13 +733,11 @@ function appendVideoPreview(){
 				console.log('>>>>>>>>>>>>>2');
 				
 				var head2 = createPreViewCheckboxObj.parentElement.parentElement.parentElement.getElementsByTagName("iframe")[0].contentDocument.getElementsByTagName("style");
-				console.log(head2[0]);
 				
 				console.log(head2[1]);
-				console.log(head2[2]);
 				
 				
-				
+				console.log(head2[1].adbg);
 				
 				
 				var head = createPreViewCheckboxObj.parentElement.parentElement.parentElement.getElementsByTagName("iframe")[0].contentDocument.getElementsByTagName("head")[0];
