c98a7830335bbce6c45b8b5de27367b0d0989117
alexchen <alexchen@alexchen6265.online.com.tw>
2017-11-04 16:36:14 +0800

    update

--- WebContent/html/js/ad/pcvideo_action_preview.js
+++ WebContent/html/js/ad/pcvideo_action_preview.js
@@ -70,7 +70,7 @@
 					var ypos;
 
 					var adbg = "";
-					var poster = "img/video/missdior.gif"
+					//var poster = "img/video/missdior.gif"
 					
 					if (vdoh/adw<ratio){
 						vdow=vdoh/ratio;
@@ -89,8 +89,8 @@
 					
 					var adbg = this.contentDocument.childNodes[0].querySelector('.adbg');
 					adbg.setAttribute("style", "background-color:black;");
-					this.width = 230;
-					this.height = adh;
+//					this.width = 230;
+//					this.height = adh;
 				}
 			};
 		}
