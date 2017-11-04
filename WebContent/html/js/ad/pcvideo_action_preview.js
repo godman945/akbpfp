b3bc0c03e1360674c5b38804ba6a5aba9037b7a0
alexchen <alexchen@alexchen6265.online.com.tw>
2017-11-04 17:20:19 +0800

    update

--- WebContent/html/js/ad/pcvideo_action_preview.js
+++ WebContent/html/js/ad/pcvideo_action_preview.js
@@ -56,13 +56,19 @@
 					var css = document.createElement("style");
 					css.type = "text/css";
 					
-					var adratio = null;
-					if(this.width > this.height){
-						adratio = this.height / this.width;
-					}else{
-						adratio = this.width / this.height;
+					var w = null;
+					var h = null;
+					if(this.width == 950){
+						w = 950 - 310;
+						h = 390;
 					}
-						
+					this.width = w / 250;
+					this.height = h /this.width;
+					
+					var adratio = this.height / this.width;
+					
+				
+					
 					var adw = 250;
 					var adh = adw * adratio;
 					
