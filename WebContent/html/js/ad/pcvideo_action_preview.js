c681f151a7e7440d53bfc0accd1257ad6023aedf
alexchen <alexchen@alexchen6265.online.com.tw>
2017-11-04 15:14:37 +0800

    update

--- WebContent/html/js/ad/pcvideo_action_preview.js
+++ WebContent/html/js/ad/pcvideo_action_preview.js
@@ -92,7 +92,7 @@
 					css.innerHTML = ".adw{width:"+adw+"px}.adh{height:"+adh+"px}.vdow{width:"+vdow+"}.xpos{left:"+xpos+"px}.ypos{top:"+ypos+"px}";
 					this.contentDocument.childNodes[0].getElementsByTagName("head")[0].appendChild(css);
 					
-					var adbg = this.contentDocument.body.childNodes[0].contentWindow.document.querySelector('.adbg');
+					var adbg = this.contentDocument.childNodes[0].querySelector('.adbg');
 					console.log(adbg);
 					this.width = 230;
 					this.height = adh;
