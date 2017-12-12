dccd6b07ac344df96184388ce431a5d0cec373ae
alexchen <alexchen@alexchen6265.online.com.tw>
2017-12-12 13:19:34 +0800

    update

--- WebContent/html/js/ad/ad.add.video.js
+++ WebContent/html/js/ad/ad.add.video.js
@@ -105,7 +105,7 @@ $(document).ready(function(){
 					adPreviewVideoData = result;
 					var adTitle = adPreviewVideoData.adTitle;
 					/**影片標題*/
-					$('#adTitle').html('<div style="display:inline;font-size:15px;">Youtube標題:</div>'+adTitle);
+					$('#adTitle').html('<div style="display:inline;font-size:10px;">'+adTitle+'</div>');
 					
 					/**影片預設尺寸*/
 					if(adPreviewVideoData != null){
