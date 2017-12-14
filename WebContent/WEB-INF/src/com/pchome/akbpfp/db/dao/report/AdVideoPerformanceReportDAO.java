b81e440ebf9aae86e9250548cca6bc8bab4ed77b
alexchen <alexchen@alexchen6265.online.com.tw>
2017-12-14 17:00:38 +0800

    fix reportVideoPerformance search

--- WebContent/WEB-INF/src/com/pchome/akbpfp/db/dao/report/AdVideoPerformanceReportDAO.java
+++ WebContent/WEB-INF/src/com/pchome/akbpfp/db/dao/report/AdVideoPerformanceReportDAO.java
@@ -20,7 +20,7 @@ public class AdVideoPerformanceReportDAO extends BaseDAO<PfpAdVideoReport, Integ
 		sql.append(" ( ");
 		sql.append(" SELECT vr.ad_seq,  ");
 		sql.append(" a.ad_status,  ");
-		sql.append(" IFNULL((select d.ad_detail_content from pfp_ad_detail d where d.ad_detail_id = 'content' and d.ad_seq = vr.ad_seq ),''),   ");
+		sql.append(" IFNULL((select d.ad_detail_content from pfp_ad_detail d where d.ad_detail_id = 'content' and d.ad_seq = vr.ad_seq ),'')title,   ");
 		sql.append(" vr.ad_price_type,  ");
 		sql.append(" vr.ad_pvclk_device,  ");
 		sql.append(" SUM(vr.ad_pv),  ");
@@ -93,11 +93,11 @@ public class AdVideoPerformanceReportDAO extends BaseDAO<PfpAdVideoReport, Integ
 		}
 		if (StringUtils.isNotBlank(reportQueryConditionVO.getSearchText())) {
 			if(reportQueryConditionVO.getAdSearchWay().equals("adsearch_include")){
-				sql.append(" AND a.ad_group_name like :searchText ");
+				sql.append(" AND a.title like :searchText ");
 			}else if(reportQueryConditionVO.getAdSearchWay().equals("adsearch_begin")){
-				sql.append(" AND a.ad_group_name like :searchText");
+				sql.append(" AND a.title like :searchText");
 			}else if(reportQueryConditionVO.getAdSearchWay().equals("adsearch_same")){
-				sql.append(" AND a.ad_group_name  = :searchText ");
+				sql.append(" AND a.title  = :searchText ");
 			}
 		}
 		sql.append(" ORDER BY a.ad_video_date DESC  ");
@@ -142,7 +142,7 @@ public class AdVideoPerformanceReportDAO extends BaseDAO<PfpAdVideoReport, Integ
 		sql.append(" ( ");
 		sql.append(" SELECT vr.ad_seq,  ");
 		sql.append(" a.ad_status,  ");
-		sql.append(" IFNULL((select d.ad_detail_content from pfp_ad_detail d where d.ad_detail_id = 'content' and d.ad_seq = vr.ad_seq ),''),   ");
+		sql.append(" IFNULL((select d.ad_detail_content from pfp_ad_detail d where d.ad_detail_id = 'content' and d.ad_seq = vr.ad_seq ),'')title,   ");
 		sql.append(" vr.ad_price_type,  ");
 		sql.append(" vr.ad_pvclk_device,  ");
 		sql.append(" SUM(vr.ad_pv),  ");
@@ -215,11 +215,11 @@ public class AdVideoPerformanceReportDAO extends BaseDAO<PfpAdVideoReport, Integ
 		}
 		if (StringUtils.isNotBlank(reportQueryConditionVO.getSearchText())) {
 			if(reportQueryConditionVO.getAdSearchWay().equals("adsearch_include")){
-				sql.append(" AND a.ad_group_name like :searchText ");
+				sql.append(" AND a.title like :searchText ");
 			}else if(reportQueryConditionVO.getAdSearchWay().equals("adsearch_begin")){
-				sql.append(" AND a.ad_group_name like :searchText");
+				sql.append(" AND a.title like :searchText");
 			}else if(reportQueryConditionVO.getAdSearchWay().equals("adsearch_same")){
-				sql.append(" AND a.ad_group_name  = :searchText ");
+				sql.append(" AND a.title  = :searchText ");
 			}
 		}
 		sql.append(" ORDER BY a.ad_video_date DESC  ");
@@ -252,129 +252,13 @@ public class AdVideoPerformanceReportDAO extends BaseDAO<PfpAdVideoReport, Integ
 			}
 		}
 		return query.list();
-		
-//		StringBuilder sql = new StringBuilder();
-//		sql.append(" SELECT a.ad_seq, ");
-//		sql.append("  a.ad_video_date, ");
-//		sql.append("  a.ad_group_name, ");
-//		sql.append("  a.ad_price_type, ");
-//		sql.append("  SUM(a.ad_pv), ");
-//		sql.append("  SUM(a.ad_view), ");
-//		sql.append("  TRUNCATE((SUM(a.ad_view) / SUM(a.ad_pv)) * 100, 2) ad_view_ratings, ");
-//		sql.append("  IFNULL(TRUNCATE(SUM(a.ad_price) / SUM(a.ad_view), 2), 0), ");
-//		sql.append("  TRUNCATE(SUM(a.ad_price) / (SUM(a.ad_pv) / 1000), 2)thousands_cost, ");
-//		sql.append("  SUM(a.ad_price)ad_price, ");
-//		sql.append("  TRUNCATE((SUM(a.ad_video_process_100) / SUM(a.ad_pv)) * 100, 2), ");
-//		sql.append("  SUM(a.ad_click)ad_click, ");
-//		sql.append("  SUM(a.ad_video_music)ad_video_music, ");
-//		sql.append("  SUM(a.ad_video_replay)ad_video_replay, ");
-//		sql.append("  a.ad_action_name, ");
-//		sql.append("  SUM(a.ad_video_process_25), ");
-//		sql.append("  SUM(a.ad_video_process_50), ");
-//		sql.append("  SUM(a.ad_video_process_75), ");
-//		sql.append("  SUM(a.ad_video_process_100), ");
-//		sql.append("  a.video_size, ");
-//		sql.append("  SUM(IFNULL(ud.uniq_count,0))uniq_count ");
-//		sql.append(" FROM ( ");
-//		sql.append(" SELECT vr.ad_seq, ");
-//		sql.append(" vr.ad_video_date, ");
-//		sql.append(" g.ad_group_name, ");
-//		sql.append(" vr.ad_price_type, ");
-//		sql.append(" SUM(vr.ad_pv)ad_pv, ");
-//		sql.append(" SUM(vr.ad_view)ad_view, ");
-//		sql.append(" TRUNCATE((SUM(vr.ad_view) / SUM(vr.ad_pv)) * 100, 2) ad_view_ratings, ");
-//		sql.append(" IFNULL(TRUNCATE(SUM(vr.ad_price) / SUM(vr.ad_view), 2), 0), ");
-//		sql.append(" TRUNCATE(SUM(vr.ad_price) / (SUM(vr.ad_pv) / 1000), 2)thousands_cost, ");
-//		sql.append(" SUM(vr.ad_price)ad_price, ");
-//		sql.append(" TRUNCATE((SUM(vr.ad_video_process_100) / SUM(vr.ad_pv)) * 100, 2), ");
-//		sql.append(" SUM(vr.ad_clk)ad_click, ");
-//		sql.append(" SUM(vr.ad_video_music)ad_video_music, ");
-//		sql.append(" SUM(vr.ad_video_replay)ad_video_replay, ");
-//		sql.append(" (SELECT aa.ad_action_name ");
-//		sql.append(" FROM pfp_ad_action aa ");
-//		sql.append(" WHERE aa.ad_action_seq = g.ad_action_seq)ad_action_name, ");
-//		sql.append(" SUM(vr.ad_video_process_25)ad_video_process_25, ");
-//		sql.append(" SUM(vr.ad_video_process_50)ad_video_process_50, ");
-//		sql.append(" SUM(vr.ad_video_process_75)ad_video_process_75, ");
-//		sql.append(" SUM(vr.ad_video_process_100)ad_video_process_100, ");
-//		sql.append(" (SELECT dd.ad_detail_content ");
-//		sql.append("  FROM pfp_ad_detail dd ");
-//		sql.append(" WHERE dd.ad_seq = vr.ad_seq ");
-//		sql.append("   AND dd.ad_detail_id = 'video_size')video_size, ");
-//		sql.append("   vr.ad_pvclk_device ");
-//		sql.append(" FROM pfp_ad_video_report vr, ");
-//		sql.append(" pfp_ad a, ");
-//		sql.append(" pfp_ad_group g ");
-//		sql.append(" WHERE 1 = 1 ");
-//		sql.append(" AND vr.customer_info_id = :customerInfoId ");
-//		sql.append(" AND vr.ad_seq = a.ad_seq ");
-//		sql.append(" AND g.ad_group_seq = a.ad_group_seq ");
-//		sql.append(" AND vr.ad_video_date >= :startDate ");
-//		sql.append(" AND vr.ad_video_date <= :endDate ");
-//		sql.append(" GROUP BY vr.ad_seq, ");
-//		sql.append(" vr.ad_video_date, ");
-//		sql.append(" video_size, ");
-//		sql.append(" vr.ad_price_type ");
-//		sql.append(" ORDER BY vr.ad_video_date ASC )a left join adm_uniq_data ud on ud.record_date = a.ad_video_date and ud.uniq_name = a.ad_seq ");
-//		sql.append(" where 1 = 1 ");
-//		if(StringUtils.isNotBlank(reportQueryConditionVO.getAdPriceType())){
-//			sql.append(" AND a.ad_price_type = :adPriceType "); 
-//		}
-//		if(StringUtils.isNotBlank(reportQueryConditionVO.getAdSize())){
-//			sql.append(" AND a.video_size = :adSize "); 
-//		}
-//		if(StringUtils.isNotBlank(reportQueryConditionVO.getAdPvclkDevice())){
-//			sql.append(" AND a.ad_pvclk_device = :adPvclkDevice "); 
-//		}
-//		if (StringUtils.isNotBlank(reportQueryConditionVO.getSearchText())) {
-//			if(reportQueryConditionVO.getAdSearchWay().equals("adsearch_include")){
-//				sql.append(" AND a.ad_group_name like :searchText ");
-//			}else if(reportQueryConditionVO.getAdSearchWay().equals("adsearch_begin")){
-//				sql.append(" AND a.ad_group_name like :searchText");
-//			}else if(reportQueryConditionVO.getAdSearchWay().equals("adsearch_same")){
-//				sql.append(" AND a.ad_group_name  = :searchText ");
-//			}
-//		}
-//		if(type.equals("chart")){
-//			sql.append(" GROUP BY a.ad_video_date");
-//			log.info(">>>>>>chart sql  = " + sql.toString());
-//		}else{
-//			sql.append(" GROUP  BY a.ad_seq ");
-//			log.info(">>>>>>count sql  = " + sql.toString());
-//		}
-//		
-//		Query query =  super.getSession().createSQLQuery(sql.toString());
-//		
-//		query.setParameter("customerInfoId", reportQueryConditionVO.getCustomerInfoId());
-//		if(StringUtils.isNotBlank(reportQueryConditionVO.getStartDate()) && StringUtils.isNotBlank(reportQueryConditionVO.getEndDate())){
-//			query.setParameter("startDate", reportQueryConditionVO.getStartDate());
-//			query.setParameter("endDate", reportQueryConditionVO.getEndDate());
-//		}
-//		if(StringUtils.isNotBlank(reportQueryConditionVO.getAdPriceType())){
-//			query.setParameter("adPriceType", reportQueryConditionVO.getAdPriceType());
-//		}
-//		if(StringUtils.isNotBlank(reportQueryConditionVO.getAdSize())){
-//			query.setParameter("adSize", reportQueryConditionVO.getAdSize());
-//		}
-//		if(StringUtils.isNotBlank(reportQueryConditionVO.getAdPvclkDevice())){
-//			query.setParameter("adPvclkDevice", reportQueryConditionVO.getAdPvclkDevice());
-//		}
-//		if (StringUtils.isNotBlank(reportQueryConditionVO.getSearchText())) {
-//			if(reportQueryConditionVO.getAdSearchWay().equals("adsearch_include")){
-//				query.setParameter("searchText", "%"+reportQueryConditionVO.getSearchText()+"%");
-//			}else if(reportQueryConditionVO.getAdSearchWay().equals("adsearch_begin")){
-//				query.setParameter("searchText", reportQueryConditionVO.getSearchText()+"%");
-//			}else if(reportQueryConditionVO.getAdSearchWay().equals("adsearch_same")){
-//				query.setParameter("searchText", reportQueryConditionVO.getSearchText());
-//			}
-//		}
-//		return query.list();
 	}
+	
 	public List<Object> getReportChart(ReportQueryConditionVO reportQueryConditionVO) throws Exception {
 		StringBuilder sql = new StringBuilder();
 		sql.append(" SELECT a.ad_seq, ");
 		sql.append("  a.ad_video_date, ");
-		sql.append("  a.ad_group_name, ");
+		sql.append("  a.title,   ");
 		sql.append("  a.ad_price_type, ");
 		sql.append("  SUM(a.ad_pv), ");
 		sql.append("  SUM(a.ad_view), ");
@@ -396,7 +280,7 @@ public class AdVideoPerformanceReportDAO extends BaseDAO<PfpAdVideoReport, Integ
 		sql.append(" FROM ( ");
 		sql.append(" SELECT vr.ad_seq, ");
 		sql.append(" vr.ad_video_date, ");
-		sql.append(" g.ad_group_name, ");
+		sql.append(" IFNULL((select d.ad_detail_content from pfp_ad_detail d where d.ad_detail_id = 'content' and d.ad_seq = vr.ad_seq ),'')title, ");
 		sql.append(" vr.ad_price_type, ");
 		sql.append(" SUM(vr.ad_pv)ad_pv, ");
 		sql.append(" SUM(vr.ad_view)ad_view, ");
@@ -446,11 +330,11 @@ public class AdVideoPerformanceReportDAO extends BaseDAO<PfpAdVideoReport, Integ
 		}
 		if (StringUtils.isNotBlank(reportQueryConditionVO.getSearchText())) {
 			if(reportQueryConditionVO.getAdSearchWay().equals("adsearch_include")){
-				sql.append(" AND a.ad_group_name like :searchText ");
+				sql.append(" AND a.title like :searchText ");
 			}else if(reportQueryConditionVO.getAdSearchWay().equals("adsearch_begin")){
-				sql.append(" AND a.ad_group_name like :searchText");
+				sql.append(" AND a.title like :searchText");
 			}else if(reportQueryConditionVO.getAdSearchWay().equals("adsearch_same")){
-				sql.append(" AND a.ad_group_name  = :searchText ");
+				sql.append(" AND a.title  = :searchText ");
 			}
 		}
 		sql.append(" GROUP BY a.ad_video_date");
