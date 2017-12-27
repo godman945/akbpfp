3733eb43c5b0172a0caca169faad3d32dee84934
alexchen <alexchen@alexchen6265.online.com.tw>
2017-12-27 13:25:00 +0800

    update

--- WebContent/html/js/ad/ad.add.video.js
+++ WebContent/html/js/ad/ad.add.video.js
@@ -677,7 +677,6 @@ function autoPreview(objData){
 	iframeInfoMap["iframe"+1] = {width:336,height:280};
 	iframeInfoMap["iframe"+2] = {width:640,height:390};
 	var url = objData.previewUrl;
-	
 	$.each(iframeInfoMap, function(key, obj) {
 		var a = 
 			'<div class="v_box">'+
@@ -686,7 +685,7 @@ function autoPreview(objData){
 			   '</div>'+
 			   '<div  class="v_preview box_a_style">'+
 			   '<iframe class="akb_iframe"  scrolling="no" frameborder="0" marginwidth="0" marginheight="0" vspace="0" hspace="0" id="pchome8044_ad_frame1" width="'+obj.width+'" height="'+obj.height+'" allowtransparency="true" allowfullscreen="true"' +
-			   'src="adVideoPreview.html?adPreviewVideoURL='+encodeURIComponent(url)+'&adPreviewVideoBgImg='+""+'"></iframe>';
+			   'src="adVideoPreview.html?adPreviewVideoURL='+encodeURIComponent(url)+'&adPreviewVideoBgImg='+""+'"></iframe>'+
 			   '</div>'+
 			'</div>';
 			$("#preViewArea").append(a);
