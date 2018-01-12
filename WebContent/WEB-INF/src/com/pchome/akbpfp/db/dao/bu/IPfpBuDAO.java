12251cab3d0ea67bf158039240079f6cacbce465
alexchen <alexchen@alexchen6265.online.com.tw>
2018-01-12 17:41:20 +0800

    init meger pfp_master_beta by akbpfp_video_develop

--- WebContent/WEB-INF/src/com/pchome/akbpfp/db/dao/bu/IPfpBuDAO.java
+++ WebContent/WEB-INF/src/com/pchome/akbpfp/db/dao/bu/IPfpBuDAO.java
@@ -8,6 +8,6 @@ import com.pchome.akbpfp.db.pojo.PfpBuAccount;
 public interface IPfpBuDAO extends IBaseDAO<PfpBuAccount,Integer>{
 
 	public List<PfpBuAccount> findPfpBuAccountByBuId(String buId) throws Exception;
-	
+
 	public List<PfpBuAccount> findPfpBuAccountByMemberId(String memberId) throws Exception;
 }
