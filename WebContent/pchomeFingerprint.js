dfdb7f10ccea720e29ea93b4f3cbb68548ae5aad
alexchen <alexchen@alexchen6265.online.com.tw>
2018-02-08 10:51:12 +0800

    update

--- WebContent/pchomeFingerprint.js
+++ WebContent/pchomeFingerprint.js
@@ -38,6 +38,59 @@
             }
             return target
         },
+        get: function (done) {
+            var that = this
+            var keys = {
+              data: [],
+              addPreprocessedComponent: function (pair) {
+                var componentValue = pair.value
+                if (typeof that.options.preprocessor === 'function') {
+                  componentValue = that.options.preprocessor(pair.key, componentValue)
+                }
+                keys.data.push({key: pair.key, value: componentValue})
+              }
+            }
+            keys = this.userAgentKey(keys)
+            keys = this.languageKey(keys)
+            keys = this.colorDepthKey(keys)
+            keys = this.deviceMemoryKey(keys)
+            keys = this.pixelRatioKey(keys)
+            keys = this.hardwareConcurrencyKey(keys)
+            keys = this.screenResolutionKey(keys)
+            keys = this.availableScreenResolutionKey(keys)
+            keys = this.timezoneOffsetKey(keys)
+            keys = this.sessionStorageKey(keys)
+            keys = this.localStorageKey(keys)
+            keys = this.indexedDbKey(keys)
+            keys = this.addBehaviorKey(keys)
+            keys = this.openDatabaseKey(keys)
+            keys = this.cpuClassKey(keys)
+            keys = this.platformKey(keys)
+            keys = this.doNotTrackKey(keys)
+            keys = this.pluginsKey(keys)
+            keys = this.canvasKey(keys)
+            keys = this.webglKey(keys)
+            keys = this.webglVendorAndRendererKey(keys)
+            keys = this.adBlockKey(keys)
+            keys = this.hasLiedLanguagesKey(keys)
+            keys = this.hasLiedResolutionKey(keys)
+            keys = this.hasLiedOsKey(keys)
+            keys = this.hasLiedBrowserKey(keys)
+            keys = this.touchSupportKey(keys)
+            keys = this.customEntropyFunction(keys)
+            this.fontsKey(keys, function (newKeys) {
+              var values = []
+              that.each(newKeys.data, function (pair) {
+                var value = pair.value
+                if (value && typeof value.join === 'function') {
+                  value = value.join(';')
+                }
+                values.push(value)
+              })
+              var murmur = that.x64hash128(values.join('~~~'), 31)
+              return done(murmur, newKeys.data)
+            })
+          },
         getCanvasFp: function() {
             var result = [];
             var canvas = document.createElement('canvas');
