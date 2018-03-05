4b0b11aac28a284180b03a4f67b4da3bcbafb894
alexchen <alexchen@alexchen6265.online.com.tw>
2018-03-05 11:20:12 +0800

    update

--- WebContent/html/js/ad/ad.add.video.js
+++ WebContent/html/js/ad/ad.add.video.js
@@ -800,10 +800,11 @@ function autoPreview(objData){
 			
 			iframe.onload = function() { 
 				alert('myframe is loaded'); 
+				$("#preViewArea").append(iframe);
 			};
 			iframe.src = encodeURI('adVideoModel.html?adPreviewVideoURL='+url+'&adPreviewVideoBgImg=&realUrl=&resize=true');
 //			document.body.appendChild(iframe);
-			$("#preViewArea").append(iframe);
+//			$("#preViewArea").append(iframe);
 			
 			
 //			$("#preViewArea").append('<iframe class="akb_iframe" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" vspace="0" hspace="0" id="pchome8044_ad_frame1" width="'+obj.width+'" height="'+obj.height+'" allowtransparency="true" allowfullscreen="true" src="adVideoModel.html?adPreviewVideoURL='+url+'&adPreviewVideoBgImg=&realUrl=&resize=true"></iframe>');
