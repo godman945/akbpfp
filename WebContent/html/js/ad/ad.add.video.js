745d5df23db4ff730bed59ceeeee37dc448f11db
alexchen <alexchen@alexchen6265.online.com.tw>
2018-03-05 10:55:08 +0800

    update

--- WebContent/html/js/ad/ad.add.video.js
+++ WebContent/html/js/ad/ad.add.video.js
@@ -779,21 +779,21 @@ function autoPreview(objData){
 			
 			console.log('>>>>>>>>>>>>>>>>非直立影音');
 			console.log('>>>>>>>>>>>>>>>>key:'+key);
-			console.log('>>>>>>>>>>>>>>>>obj'+obj);
+			console.log(obj);
 			
-			var a = 
-				'<div class="v_box">'+
-				   '<div class="">'+
-				      '<span><input type="checkbox" name="checkbox" id="checkbox_'+obj.width+obj.height+'" checked onclick="checkVideo(this)"/>'+obj.width+'x'+obj.height+'</span>'+
-				   '</div>'+
-				   '<div  class="v_preview box_a_style">'+
-				   '<iframe class="akb_iframe" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" vspace="0" hspace="0" id="pchome8044_ad_frame1" width="'+obj.width+'" height="'+obj.height+'" allowtransparency="true" allowfullscreen="true" src="adVideoModel.html?adPreviewVideoURL='+url+'&adPreviewVideoBgImg=&realUrl=&resize=true"></iframe>'+
-				   '</div>'+
-				'</div>';
-				$("#preViewArea").append(a);
-				
-				
-				return false;
+//			var a = 
+//				'<div class="v_box">'+
+//				   '<div class="">'+
+//				      '<span><input type="checkbox" name="checkbox" id="checkbox_'+obj.width+obj.height+'" checked onclick="checkVideo(this)"/>'+obj.width+'x'+obj.height+'</span>'+
+//				   '</div>'+
+//				   '<div  class="v_preview box_a_style">'+
+//				   '<iframe class="akb_iframe" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" vspace="0" hspace="0" id="pchome8044_ad_frame1" width="'+obj.width+'" height="'+obj.height+'" allowtransparency="true" allowfullscreen="true" src="adVideoModel.html?adPreviewVideoURL='+url+'&adPreviewVideoBgImg=&realUrl=&resize=true"></iframe>'+
+//				   '</div>'+
+//				'</div>';
+//				$("#preViewArea").append(a);
+			var a = '<iframe class="akb_iframe" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" vspace="0" hspace="0" id="pchome8044_ad_frame1" width="'+obj.width+'" height="'+obj.height+'" allowtransparency="true" allowfullscreen="true" src="adVideoModel.html?adPreviewVideoURL='+url+'&adPreviewVideoBgImg=&realUrl=&resize=true"></iframe>';
+			$("#preViewArea").append(a);
+			return false;
 				
 		});
 		$(".adVideoCheckArea").css('display','');
