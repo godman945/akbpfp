a36673f1e5b166f546db1acc69539d7534c16760
alexchen <alexchen@alexchen6265.online.com.tw>
2018-03-05 11:25:04 +0800

    update

--- WebContent/html/js/ad/ad.add.video.js
+++ WebContent/html/js/ad/ad.add.video.js
@@ -797,14 +797,15 @@ function autoPreview(objData){
 			var iframe = document.createElement('iframe');
 //			iframe.src = 'data:text/html;charset=utf-8,' + encodeURI('adVideoModel.html?adPreviewVideoURL='+url+'&adPreviewVideoBgImg=&realUrl=&resize=true');
 			
-			
+			var utl = 'adVideoModel.html?adPreviewVideoURL='+url+'&adPreviewVideoBgImg=&realUrl=&resize=true';
 			iframe.onload = function() { 
 				alert('myframe is loaded'); 
-				$("#preViewArea").append(iframe);
+//				$("#preViewArea").append(iframe);
+				iframe.src = utl;
 			};
-			iframe.src = encodeURI('adVideoModel.html?adPreviewVideoURL='+url+'&adPreviewVideoBgImg=&realUrl=&resize=true');
+			iframe.src = '';
 //			document.body.appendChild(iframe);
-//			$("#preViewArea").append(iframe);
+			$("#preViewArea").append(iframe);
 			
 			
 //			$("#preViewArea").append('<iframe class="akb_iframe" scrolling="no" frameborder="0" marginwidth="0" marginheight="0" vspace="0" hspace="0" id="pchome8044_ad_frame1" width="'+obj.width+'" height="'+obj.height+'" allowtransparency="true" allowfullscreen="true" src="adVideoModel.html?adPreviewVideoURL='+url+'&adPreviewVideoBgImg=&realUrl=&resize=true"></iframe>');
