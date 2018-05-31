3ac07492e8677b5bc952324eaf47ca79cf02887d
alexchen <alexchen@alexchen11248.online.com.tw>
2018-05-31 16:10:16 +0800

    update

---
+++ WebContent/WEB-INF/src/com/pchome/enumerate/bu/EnumBuGift.java
@@ -0,0 +1,18 @@
+package com.pchome.enumerate.bu;
+
+public enum EnumBuGift {
+
+	GIFT_NO("STTBDAEFJ8");
+	
+	private final String content;
+	
+	private EnumBuGift(String content){
+		this.content = content;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+
+}
