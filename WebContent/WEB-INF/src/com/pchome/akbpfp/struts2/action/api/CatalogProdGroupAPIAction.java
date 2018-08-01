7563a4ab3e51b914da6fab62a4d347ab55e87095
bessie <bessie@bessie6166.online.com.tw>
2018-08-01 10:23:03 +0800

    update

--- WebContent/WEB-INF/src/com/pchome/akbpfp/struts2/action/api/CatalogProdGroupAPIAction.java
+++ WebContent/WEB-INF/src/com/pchome/akbpfp/struts2/action/api/CatalogProdGroupAPIAction.java
@@ -7,9 +7,7 @@ import com.pchome.akbpfp.struts2.BaseCookieAction;
 
 public class CatalogProdGroupAPIAction extends BaseCookieAction{
 	
-	private PfpCatalogGroupService pfpCatalogGroupService;
 //	private PfpOrderService pfpOrderService;
-	private ProdGroupFactory prodGroupFactory;
 	private String groupId;
 	private String returnJson;
 	
@@ -18,26 +16,6 @@ public class CatalogProdGroupAPIAction extends BaseCookieAction{
 		System.out.println("groupId : "+groupId);
 		
 		
-		//先依據商品組合id，撈商品組合條件組成sql
-		
-		
-		
-		
-		String catalogType = pfpCatalogGroupService.getCatalogType(groupId) ;
-		
-		AProdGroup aProdGroup = prodGroupFactory.getAProdGroupObj(catalogType);
-		
-		
-		System.out.println("父類別方法1: "+aProdGroup.getCatalogGroupSQL());
-		System.out.println("子類別方法1: "+aProdGroup.getProdGroupList(groupId));
-		
-		
-		
-		aProdGroup = prodGroupFactory.getAProdGroupObj("2");
-		System.out.println("父類別方法2: "+aProdGroup.getCatalogGroupSQL());
-		System.out.println("子類別方法2: "+aProdGroup.getProdGroupList(groupId));
-		
-		groupId = aProdGroup.getProdGroupList(groupId);
 		
 		
 		returnJson = groupId;
@@ -47,19 +25,8 @@ public class CatalogProdGroupAPIAction extends BaseCookieAction{
 	
 	
 	
-//	public void setPfpOrderService(PfpOrderService pfpOrderService) {
-//		this.pfpOrderService = pfpOrderService;
-//	}
 
 
-//	public String getGroupId() {
-//		return groupId;
-//	}
-
-
-	public void setPfpCatalogGroupService(PfpCatalogGroupService pfpCatalogGroupService) {
-		this.pfpCatalogGroupService = pfpCatalogGroupService;
-	}
 
 	public void setGroupId(String groupId) {
 		this.groupId = groupId;
@@ -75,15 +42,6 @@ public class CatalogProdGroupAPIAction extends BaseCookieAction{
 		this.returnJson = returnJson;
 	}
 
-
-
-	public void setProdGroupFactory(ProdGroupFactory prodGroupFactory) {
-		this.prodGroupFactory = prodGroupFactory;
-	}
-
-
-	
-	
 	
 	
 	
