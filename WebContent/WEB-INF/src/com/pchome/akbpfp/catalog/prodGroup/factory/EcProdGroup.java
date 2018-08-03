4312786abe0ab4bb5fa464c3be271e877952120f
bessie <bessie@bessie6166.online.com.tw>
2018-08-03 14:05:30 +0800

    update

--- WebContent/WEB-INF/src/com/pchome/akbpfp/catalog/prodGroup/factory/EcProdGroup.java
+++ WebContent/WEB-INF/src/com/pchome/akbpfp/catalog/prodGroup/factory/EcProdGroup.java
@@ -40,9 +40,17 @@ public class EcProdGroup extends AProdGroup {
 			JSONObject prodDataJson = new JSONObject(); 
 			
 			Map obj = (Map) object;
-			prodDataJson.put("prod_url", obj.get("prod_url").toString());
+			prodDataJson.put("id", obj.get("id").toString());
+			prodDataJson.put("catalog_prod_ec_seq", obj.get("catalog_prod_ec_seq").toString());
+			prodDataJson.put("catalog_seq", obj.get("catalog_seq").toString());
 			prodDataJson.put("prod_name", obj.get("prod_name").toString());
+			prodDataJson.put("prod_title", obj.get("prod_title").toString());
+			prodDataJson.put("prod_url", obj.get("prod_url").toString());
+			prodDataJson.put("prod_price", "NT."+obj.get("prod_price").toString());
 			prodDataJson.put("prod_discount_price", "NT."+obj.get("prod_discount_price").toString());
+			prodDataJson.put("prod_stock_status", obj.get("prod_stock_status").toString());
+			prodDataJson.put("prod_use_status", obj.get("prod_use_status").toString());
+			prodDataJson.put("prod_category", obj.get("prod_category").toString());
 			
 			prodListJson.put(prodDataJson);
 		}
