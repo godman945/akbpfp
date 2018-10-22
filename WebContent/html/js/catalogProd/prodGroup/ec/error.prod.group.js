d694bda703fb16f81a60a4edb1257bb285101f3b
bessie <bessie@bessie6166.online.com.tw>
2018-10-22 17:44:20 +0800

    update

---
+++ WebContent/html/js/catalogProd/prodGroup/ec/error.prod.group.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function(){
+	//切換商品目錄
+	$("#catalog").change(function() {
+		location.href = "prodListCardStyleView.html?catalogSeq="+ $('#catalog option:selected').val() + "&currentPage=1&pageSizeSelected=10";
+	 });
+});
+
