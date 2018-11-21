1db4a223e7a4ca63934175a13b35e6143e2a17f3
twkuo <twkuo@twkuo6164.online.com.tw>
2018-11-21 14:51:24 +0800

    update autojob

--- WebContent/WEB-INF/src/com/pchome/akbpfp/struts2/action/catalog/uploadList/PfpCatalogUploadListAction.java
+++ WebContent/WEB-INF/src/com/pchome/akbpfp/struts2/action/catalog/uploadList/PfpCatalogUploadListAction.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
