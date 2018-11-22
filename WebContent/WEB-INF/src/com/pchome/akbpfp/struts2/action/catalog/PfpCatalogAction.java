75667d5fd738cda46f6334aa1e70de78b3ca9fdf
twkuo <twkuo@twkuo6164.online.com.tw>
2018-11-22 18:25:17 +0800

    update add catalog check catalogName

--- WebContent/WEB-INF/src/com/pchome/akbpfp/struts2/action/catalog/PfpCatalogAction.java
+++ WebContent/WEB-INF/src/com/pchome/akbpfp/struts2/action/catalog/PfpCatalogAction.java
@@ -37,7 +37,7 @@ public class PfpCatalogAction extends BaseCookieAction{
 	private IPfpCatalogUploadListService pfpCatalogUploadListService;
 	private IPfpCatalogGroupItemService pfpCatalogGroupItemService;
 	private IPfpAdService pfpAdService;
-	
+	private String message = "";
 	
 	private String queryString = ""; // 預設為空
 	private int pageNo = 1;          // 初始化目前頁數
@@ -111,7 +111,12 @@ public class PfpCatalogAction extends BaseCookieAction{
 	 * @throws Exception 
 	 */
 	public String savePfpCatalog() throws Exception {
-		dataMap = new HashMap<String, Object>();
+		
+		int catalogNameCount = pfpCatalogService.checkCatalogName(catalogName, super.getCustomer_info_id());
+		if (catalogNameCount >= 1) {
+			message = "目錄名稱已重複。";
+			return INPUT;
+		}
 		
 		PfpCatalogVO vo = new PfpCatalogVO();
 		vo.setCatalogName(catalogName);
@@ -346,6 +351,10 @@ public class PfpCatalogAction extends BaseCookieAction{
 
 	public boolean isShowPromptMessage() {
 		return showPromptMessage;
+	}
+
+	public String getMessage() {
+		return message;
 	}
 
 }
