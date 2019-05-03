f128895770b535c1f1017b7be1c7988017900996
alexchen <alexchen@alexchen6264.online.com.tw>
2019-05-03 17:25:57 +0800

    update

--- src/main/java/com/pchome/akbpfp/struts2/BaseSSLAction.java
+++ src/main/java/com/pchome/akbpfp/struts2/BaseSSLAction.java
@@ -25,20 +25,36 @@ public class BaseSSLAction extends BaseCookieAction{
 		
 		if(this.isSSLUrl()){
 			// 需要轉成 https
-			log.info("request.getScheme():"+request.getScheme());
-			log.info("request.getScheme():"+request.getRequestURL());
-			log.info("request.getScheme():"+request.getHeader("referer"));
-			
-			
-			if(!request.getScheme().equals("https")){
+			if(!request.getHeader("referer").contains("pchome")){
 				if(request.getServerName().indexOf(LOCAL_DONAME) > -1){
 					redirectUrl = LOCAL_SCHEME+request.getServerName()+":"+LOCAL_PORT+request.getRequestURI();
-				}else{
-//					log.info("TEST???????????:"+"https://"+request.getServerName()+request.getRequestURI());
+				}else {
+					log.info("request.getScheme():"+request.getScheme());
+					log.info("request.getScheme():"+request.getRequestURL());
+					log.info("request.getScheme():"+request.getHeader("referer"));
+					
 					redirectUrl = "https://"+request.getServerName()+request.getRequestURI();
 					resultType = "redirectHTTPS";
 				}
 			}
+			
+			
+			
+			
+//			log.info("request.getScheme():"+request.getScheme());
+//			log.info("request.getScheme():"+request.getRequestURL());
+//			log.info("request.getScheme():"+request.getHeader("referer"));
+			
+			
+//			if(!request.getScheme().equals("https")){
+//				if(request.getServerName().indexOf(LOCAL_DONAME) > -1){
+//					redirectUrl = LOCAL_SCHEME+request.getServerName()+":"+LOCAL_PORT+request.getRequestURI();
+//				}else{
+////					log.info("TEST???????????:"+"https://"+request.getServerName()+request.getRequestURI());
+//					redirectUrl = "https://"+request.getServerName()+request.getRequestURI();
+//					resultType = "redirectHTTPS";
+//				}
+//			}
 		}else{
 			// 不需要轉成 https
 			if(!request.getScheme().equals("http")){
