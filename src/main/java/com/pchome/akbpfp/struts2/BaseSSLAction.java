2133b19f0c41dede457d7ceca1b958bbd779b8bb
alexchen <alexchen@alexchen6264.online.com.tw>
2019-05-03 16:46:59 +0800

    update

--- src/main/java/com/pchome/akbpfp/struts2/BaseSSLAction.java
+++ src/main/java/com/pchome/akbpfp/struts2/BaseSSLAction.java
@@ -23,6 +23,7 @@ public class BaseSSLAction extends BaseCookieAction{
 		
 		if(this.isSSLUrl()){
 			// 需要轉成 https
+			log.info("request.getScheme():"+request.getScheme());
 			if(!request.getScheme().equals("https")){
 				if(request.getServerName().indexOf(LOCAL_DONAME) > -1){
 					redirectUrl = LOCAL_SCHEME+request.getServerName()+":"+LOCAL_PORT+request.getRequestURI();
