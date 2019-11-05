b767020ad174ab9aed3465d21f47db85107d1a57
alexchen <alexchen@alexchen6264.online.com.tw>
2019-11-05 16:15:14 +0800

    update

--- src/main/java/alex/com/pchome/adbpfp/spring/init/MozJpegTest.java
+++ src/main/java/alex/com/pchome/adbpfp/spring/init/MozJpegTest.java
@@ -1,13 +1,16 @@
 package alex.com.pchome.adbpfp.spring.init;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.apache.commons.io.IOUtils;
 
 public class MozJpegTest {
 
 	
-	public static void alex(File file) {
+	public static void alex(File file) throws Exception {
+		Process process = null;
+		String result = "";
 		StringBuffer stringBuffer = new StringBuffer();
 		File[] files = file.listFiles();
 		if (files == null) {
@@ -16,9 +19,20 @@ public class MozJpegTest {
 		for (File f : files) {
 			if (f.isFile()) {
 				System.out.println("我是檔案>>>"+f.getAbsolutePath());
+	    		stringBuffer.setLength(0);
+	    		stringBuffer.append(" cp ").append(f.getAbsolutePath()).append(" ").append(f.getAbsolutePath().replace(f.getName(), "")).append(f.getName().replace(".jpg", "_org.jpg"));
+				process = Runtime.getRuntime().exec(new String[] { "bash", "-c", stringBuffer.toString()  });
+				result = IOUtils.toString(process.getInputStream(), "UTF-8");
+				System.out.println(">>>>>>:" + result);
+				System.out.println(IOUtils.toString(process.getErrorStream(),"UTF-8"));
+				System.out.println(IOUtils.toString(process.getInputStream(),"UTF-8"));
 				stringBuffer.setLength(0);
-	    		stringBuffer.append(" cp ").append(f.getAbsolutePath()).append(" ").append(f.getAbsolutePath().replace(f.getName(), "")).append(f.getName().replace(".jpg", "_new.jpg"));
-	    		System.out.println(stringBuffer);
+				stringBuffer.append(" /opt/mozjpeg/bin/cjpeg  -quality 75 -tune-ms-ssim   -quant-table 0      -progressive      ").append(f.getAbsolutePath()).append(" >").append(f.getAbsolutePath().replace(f.getName(), "")).append(f.getName().replace(".jpg", "_new.jpg"));
+				process = Runtime.getRuntime().exec(new String[] { "bash", "-c", stringBuffer.toString()  });
+				result = IOUtils.toString(process.getInputStream(), "UTF-8");
+				System.out.println(">>>>>>:" + result);
+				System.out.println(IOUtils.toString(process.getErrorStream(),"UTF-8"));
+				System.out.println(IOUtils.toString(process.getInputStream(),"UTF-8"));
 	    	}
 		    if (f.isDirectory()) {
 	    		alex(f);
@@ -33,6 +47,7 @@ public class MozJpegTest {
 		try {
 			System.out.println(">>>>>>>>>>>>>>>>>>>>> MozJpegTest START");
 			Process process = null;
+			String result = "";
 			StringBuffer stringBuffer = new StringBuffer();
 			File file = new File("/home/webuser/akb/_alex/AC2018011500005/");
 //			File file = new File("D:\\Users\\alexchen\\Desktop\\AAAAAAAAA\\ALEX\\");
@@ -45,22 +60,26 @@ public class MozJpegTest {
 		    for (File f : files) {
 		    	if (f.isFile()) {
 		    		System.out.println("我是檔案>>>"+f.getAbsolutePath());
-//		    		System.out.println("我是檔案999999999999>>>"+f.getName());
-		    		
-		    		
 		    		stringBuffer.setLength(0);
-		    		stringBuffer.append(" cp ").append(f.getAbsolutePath()).append(" ").append(f.getAbsolutePath().replace(f.getName(), "")).append(f.getName().replace(".jpg", "_new.jpg"));
-		    		System.out.println(stringBuffer);
-//					process = Runtime.getRuntime().exec(new String[] { "bash", "-c", stringBuffer.toString()  });
-//					String result = IOUtils.toString(process.getInputStream(), "UTF-8");
-//					System.out.println(">>>>>>:" + result);
-//					System.out.println(IOUtils.toString(process.getErrorStream(),"UTF-8"));
-//					System.out.println(IOUtils.toString(process.getInputStream(),"UTF-8"));
+		    		stringBuffer.append(" cp ").append(f.getAbsolutePath()).append(" ").append(f.getAbsolutePath().replace(f.getName(), "")).append(f.getName().replace(".jpg", "_org.jpg"));
+					process = Runtime.getRuntime().exec(new String[] { "bash", "-c", stringBuffer.toString()  });
+					result = IOUtils.toString(process.getInputStream(), "UTF-8");
+					System.out.println(">>>>>>:" + result);
+					System.out.println(IOUtils.toString(process.getErrorStream(),"UTF-8"));
+					System.out.println(IOUtils.toString(process.getInputStream(),"UTF-8"));
+					stringBuffer.setLength(0);
+					stringBuffer.append(" /opt/mozjpeg/bin/cjpeg  -quality 75 -tune-ms-ssim   -quant-table 0      -progressive      ").append(f.getAbsolutePath()).append(" >").append(f.getAbsolutePath().replace(f.getName(), "")).append(f.getName().replace(".jpg", "_new.jpg"));
+					process = Runtime.getRuntime().exec(new String[] { "bash", "-c", stringBuffer.toString()  });
+					result = IOUtils.toString(process.getInputStream(), "UTF-8");
+					System.out.println(">>>>>>:" + result);
+					System.out.println(IOUtils.toString(process.getErrorStream(),"UTF-8"));
+					System.out.println(IOUtils.toString(process.getInputStream(),"UTF-8"));
 		    	}
 			    if (f.isDirectory()) {
 		    		alex(f);
 		    	}
 		    }
+		    process.destroy();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
