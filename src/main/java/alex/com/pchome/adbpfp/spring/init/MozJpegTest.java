ecc8e8f6db1ff612681f8c418bb2db6fab16c91e
alexchen <alexchen@alexchen6264.online.com.tw>
2019-11-13 14:31:57 +0800

    update

--- src/main/java/alex/com/pchome/adbpfp/spring/init/MozJpegTest.java
+++ src/main/java/alex/com/pchome/adbpfp/spring/init/MozJpegTest.java
@@ -1,8 +1,14 @@
 package alex.com.pchome.adbpfp.spring.init;
 
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 
 public class MozJpegTest {
@@ -12,46 +18,56 @@ public class MozJpegTest {
 		Process process = null;
 		String result = "";
 		StringBuffer stringBuffer = new StringBuffer();
-		File[] files = file.listFiles();
-		if (files == null) {
-			return; 
-		}
-		for (File f : files) {
-			if (f.isFile()) {
-				//1.複製原檔 2.產上新檔
-	    		if(!f.getAbsolutePath().contains(".jpg")) {
+		File[] files = file.listFiles();// 獲取目錄下的所有檔案或資料夾 
+	    if (files == null) {// 如果目錄為空，直接退出 
+	      return; 
+	    }
+	    for (File f : files) {
+	    	if (f.isFile()) {
+	    		//1.複製原檔 2.產上新檔
+	    		if(f.getAbsolutePath().contains(".gif")) {
 	    			continue;
 	    		}
-	    		if(f.getAbsolutePath().contains("_bk.jpg")) {
-	    			continue;
-	    		}
-	    		if(new File(f.getAbsolutePath().replace(f.getName(), "")+f.getName().replace(".jpg", "_bk.jpg")).exists()) {
-	    			continue;
-	    		}
-	    		System.out.println("我是檔案>>>"+f.getAbsolutePath());
-	    		stringBuffer.setLength(0);
-	    		stringBuffer.append(" cp ").append(f.getAbsolutePath()).append(" ").append(f.getAbsolutePath().replace(f.getName(), "")).append(f.getName().replace(".jpg", "_bk.jpg"));
-	    		System.out.println(">>>>>>>>>>command:"+stringBuffer.toString());
-	    		process = Runtime.getRuntime().exec(new String[] { "bash", "-c", stringBuffer.toString()  });
-	    		result = IOUtils.toString(process.getInputStream(), "UTF-8");
+	    		String fileExtensionName = f.getAbsolutePath().substring(f.getAbsolutePath().lastIndexOf(".")+1 , f.getAbsolutePath().length());
+	    		String filePath = f.getAbsolutePath().substring(0 , f.getAbsolutePath().lastIndexOf("\\")+1);
+	    		String fileName = f.getAbsolutePath().substring(f.getAbsolutePath().lastIndexOf("\\")+1, f.getAbsolutePath().lastIndexOf("."));
+	    		System.out.println("*****START　PROCESS*****");
+	    		System.out.println("[file]:"+f.getAbsolutePath());
+	    		System.out.println("[file path]:"+filePath);
+	    		System.out.println("[file name]:"+fileName);
+	    		System.out.println("[fileExtensionName]:"+fileExtensionName);
+	    		System.out.println("Before Size:"+f.length()/1024+"kb");
 	    		
+	    		if(fileExtensionName.toUpperCase().equals("PNG")) {
+	    			ByteArrayInputStream imageStream = new ByteArrayInputStream(FileUtils.readFileToByteArray(f));
+	    			BufferedImage bufferedImage = ImageIO.read(imageStream);
+	    			BufferedImage newBufferedImage = new BufferedImage(bufferedImage.getWidth(), bufferedImage.getHeight(), BufferedImage.TYPE_INT_RGB);
+	    			newBufferedImage.createGraphics().drawImage(bufferedImage, 0, 0, Color.WHITE, null);
+	    			ImageIO.write(newBufferedImage, "jpg", new File(filePath+fileName+".jpg"));
+	    			f.delete();
+	    		}
 	    		stringBuffer.setLength(0);
-				stringBuffer.append(" /opt/mozjpeg/bin/cjpeg  -quality 75 -tune-ms-ssim   -quant-table 0      -progressive      ").append(f.getAbsolutePath()).append(" > ").append(f.getAbsolutePath().replace(f.getName(), "")).append(f.getName().replace(".jpg", "_resize.jpg"));
-				System.out.println(">>>>>>>>>>command:"+stringBuffer.toString());
+				stringBuffer.append(" /opt/mozjpeg/bin/cjpeg  -quality 75 -tune-ms-ssim   -quant-table 0      -progressive      ").append(filePath+fileName+".jpg").append(" >").append(filePath+fileName+"[RESIZE].jpg");
 				process = Runtime.getRuntime().exec(new String[] { "bash", "-c", stringBuffer.toString()  });
 				result = IOUtils.toString(process.getInputStream(), "UTF-8");
-				
+				System.out.println(">>>>>>>>>>command:"+stringBuffer.toString());
 				
 				stringBuffer.setLength(0);
-				stringBuffer.append(" cp ").append(f.getAbsolutePath().replace(f.getName(), "")).append(f.getName().replace(".jpg", "_resize.jpg")).append(" ").append(f.getAbsolutePath());
-				System.out.println(">>>>>>>>>>command:"+stringBuffer.toString());
+				stringBuffer.append(" mv ").append(filePath+fileName+"[RESIZE].jpg").append(" ").append(filePath+fileName+".jpg");
 				process = Runtime.getRuntime().exec(new String[] { "bash", "-c", stringBuffer.toString()  });
 				result = IOUtils.toString(process.getInputStream(), "UTF-8");
+				System.out.println(">>>>>>>>>>command:"+stringBuffer.toString());
+				File fileNew = new File(filePath+fileName+".jpg");
+				System.out.println("After Size:"+fileNew.length()/1024+"kb");
+				System.out.println("*****END　PROCESS*****");
 	    	}
 		    if (f.isDirectory()) {
-	    		alex(f);
+		    	String folderName = f.getPath().toString().substring(f.getPath().toString().lastIndexOf("\\")+1 , f.getPath().toString().length());
+		    	if(folderName.equals("user")) {
+		    		alex(f);
+		    	}
 	    	}
-		}
+	    }
 	}
 	
 	
@@ -65,8 +81,6 @@ public class MozJpegTest {
 			StringBuffer stringBuffer = new StringBuffer();
 			File file = new File("/home/webuser/akb/_alex/AC2018011500005/");
 //			File file = new File("D:\\Users\\alexchen\\Desktop\\AAAAAAAAA\\moz_test");
-//			List<File> fileList
-			
 			File[] files = file.listFiles();// 獲取目錄下的所有檔案或資料夾 
 		    if (files == null) {// 如果目錄為空，直接退出 
 		      return; 
@@ -74,34 +88,47 @@ public class MozJpegTest {
 		    for (File f : files) {
 		    	if (f.isFile()) {
 		    		//1.複製原檔 2.產上新檔
-		    		if(!f.getAbsolutePath().contains(".jpg")) {
+		    		if(f.getAbsolutePath().contains(".gif")) {
 		    			continue;
 		    		}
-		    		if(f.getAbsolutePath().contains("_bk.jpg")) {
-		    			continue;
-		    		}
-		    		if(new File(f.getAbsolutePath().replace(f.getName(), "")+f.getName().replace(".jpg", "_bk.jpg")).exists()) {
-		    			continue;
-		    		}
-		    		System.out.println("我是檔案>>>"+f.getAbsolutePath());
-		    		stringBuffer.setLength(0);
-		    		stringBuffer.append(" cp ").append(f.getAbsolutePath()).append(" ").append(f.getAbsolutePath().replace(f.getName(), "")).append(f.getName().replace(".jpg", "_bk.jpg"));
-		    		process = Runtime.getRuntime().exec(new String[] { "bash", "-c", stringBuffer.toString()  });
-		    		result = IOUtils.toString(process.getInputStream(), "UTF-8");
+		    		String fileExtensionName = f.getAbsolutePath().substring(f.getAbsolutePath().lastIndexOf(".")+1 , f.getAbsolutePath().length());
+		    		String filePath = f.getAbsolutePath().substring(0 , f.getAbsolutePath().lastIndexOf("\\")+1);
+		    		String fileName = f.getAbsolutePath().substring(f.getAbsolutePath().lastIndexOf("\\")+1, f.getAbsolutePath().lastIndexOf("."));
+		    		System.out.println("*****START　PROCESS*****");
+		    		System.out.println("[file]:"+f.getAbsolutePath());
+		    		System.out.println("[file path]:"+filePath);
+		    		System.out.println("[file name]:"+fileName);
+		    		System.out.println("[fileExtensionName]:"+fileExtensionName);
+		    		System.out.println("Before Size:"+f.length()/1024+"kb");
 		    		
+		    		if(fileExtensionName.toUpperCase().equals("PNG")) {
+		    			ByteArrayInputStream imageStream = new ByteArrayInputStream(FileUtils.readFileToByteArray(f));
+		    			BufferedImage bufferedImage = ImageIO.read(imageStream);
+		    			BufferedImage newBufferedImage = new BufferedImage(bufferedImage.getWidth(), bufferedImage.getHeight(), BufferedImage.TYPE_INT_RGB);
+		    			newBufferedImage.createGraphics().drawImage(bufferedImage, 0, 0, Color.WHITE, null);
+		    			ImageIO.write(newBufferedImage, "jpg", new File(filePath+fileName+".jpg"));
+		    			f.delete();
+		    		}
 		    		stringBuffer.setLength(0);
-					stringBuffer.append(" /opt/mozjpeg/bin/cjpeg  -quality 75 -tune-ms-ssim   -quant-table 0      -progressive      ").append(f.getAbsolutePath()).append(" >").append(f.getAbsolutePath()).append(" ").append(f.getAbsolutePath().replace(f.getName(), "")).append(f.getName().replace(".jpg", "_resize.jpg"));
-					process = Runtime.getRuntime().exec(new String[] { "bash", "-c", stringBuffer.toString()  });
-					result = IOUtils.toString(process.getInputStream(), "UTF-8");
-					
+					stringBuffer.append(" /opt/mozjpeg/bin/cjpeg  -quality 75 -tune-ms-ssim   -quant-table 0      -progressive      ").append(filePath+fileName+".jpg").append(" >").append(filePath+fileName+"[RESIZE].jpg");
+//					process = Runtime.getRuntime().exec(new String[] { "bash", "-c", stringBuffer.toString()  });
+//					result = IOUtils.toString(process.getInputStream(), "UTF-8");
+					System.out.println(">>>>>>>>>>command:"+stringBuffer.toString());
 					
 					stringBuffer.setLength(0);
-					stringBuffer.append(" cp ").append(f.getAbsolutePath()).append(" ").append(f.getAbsolutePath().replace(f.getName(), "")).append(f.getName().replace("_resize.jpg", ".jpg"));
-					process = Runtime.getRuntime().exec(new String[] { "bash", "-c", stringBuffer.toString()  });
-					result = IOUtils.toString(process.getInputStream(), "UTF-8");
+					stringBuffer.append(" mv ").append(filePath+fileName+"[RESIZE].jpg").append(" ").append(filePath+fileName+".jpg");
+//					process = Runtime.getRuntime().exec(new String[] { "bash", "-c", stringBuffer.toString()  });
+//					result = IOUtils.toString(process.getInputStream(), "UTF-8");
+					System.out.println(">>>>>>>>>>command:"+stringBuffer.toString());
+					File fileNew = new File(filePath+fileName+".jpg");
+					System.out.println("After Size:"+fileNew.length()/1024+"kb");
+					System.out.println("*****END　PROCESS*****");
 		    	}
 			    if (f.isDirectory()) {
-		    		alex(f);
+			    	String folderName = f.getPath().toString().substring(f.getPath().toString().lastIndexOf("\\")+1 , f.getPath().toString().length());
+			    	if(folderName.equals("user")) {
+			    		alex(f);
+			    	}
 		    	}
 		    }
 //		    process.destroy();
