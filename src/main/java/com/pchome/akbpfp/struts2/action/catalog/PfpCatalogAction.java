927aabf5d4f035f8ebda14163f068c87d89dc6f3
alexchen <alexchen@alexchen6264.online.com.tw>
2019-11-18 15:38:18 +0800

    update

--- src/main/java/com/pchome/akbpfp/struts2/action/catalog/PfpCatalogAction.java
+++ src/main/java/com/pchome/akbpfp/struts2/action/catalog/PfpCatalogAction.java
@@ -235,21 +235,13 @@ public class PfpCatalogAction extends BaseCookieAction{
 		
 		// 範例檔案放的位置
 		String serverName = super.request.getServerName();
-		log.info(">>>>>>>>>>>>>>>>>0:"+serverName);
 		if (serverName.indexOf("show.pchome.com.tw") > -1) {
-			log.info(">>>>>>>>>>>>>>>>>1:"+"https://show.pchome.com.tw/img/prod_ad_sample_file/"+fileName);
 			downloadFileStream = new URL("https://show.pchome.com.tw/img/prod_ad_sample_file/"+fileName).openStream();
-			
 		} else {
-			log.info(">>>>>>>>>>>>>>>>>2:"+"http://showstg.pchome.com.tw/pfp/img/prod_ad_sample_file/"+fileName);
 			// 本機路徑
-			downloadFileStream = new URL("http://showstg.pchome.com.tw/pfp/img/prod_ad_sample_file/"+fileName).openStream();
+			downloadFileStream = new URL("http://showstg.pchome.com.tw:8080/pfp/img/prod_ad_sample_file/"+fileName).openStream();
 			
 		}
-		log.info("********************************downloadFileStream:"+downloadFileStream == null);
-		log.info("********************************:"+IOUtils.toString(downloadFileStream));
-		
-		
 		return SUCCESS;
 	}
 	
