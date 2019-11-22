11f485a7f37113454b116cb74ce70f59262c3e27
alexchen <alexchen@alexchen6264.online.com.tw>
2019-11-22 12:16:12 +0800

    update

--- src/main/java/com/pchome/akbpfp/struts2/action/ad/AdAddAction.java
+++ src/main/java/com/pchome/akbpfp/struts2/action/ad/AdAddAction.java
@@ -320,7 +320,7 @@ public class AdAddAction extends BaseCookieAction{
 							File tmpFile = new File(imgFile);	// 設定圖片的 File 元件
 							tmpFile.renameTo(adFile);			// 把暫存圖片搬到存放區
 							//複製原圖至備份區
-							FileUtils.copyFile(adFile, new File(photoPath+"ad_backup/"+adSeq+"_backup."+FilenameUtils.getExtension(adFile.getPath())));
+							FileUtils.copyFile(adFile, new File(photoPath+"ad_backup/"+adSeq+"."+FilenameUtils.getExtension(adFile.getPath())));
 							imgDetail = photoDbPath + adFile.getName();	// 設定圖片檔存放在 DB 的路徑
 							//進行mozjpeg壓縮
 							if(FilenameUtils.getExtension(adFile.getPath()).toUpperCase().contains("JPG")) {
