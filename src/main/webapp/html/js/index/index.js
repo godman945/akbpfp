a52484956b3b7a75792a6b457fed48e0a63e110e
alexchen <alexchen@alexchen6264.online.com.tw>
2020-03-23 16:50:56 +0800

    update

--- src/main/webapp/html/js/index/index.js
+++ src/main/webapp/html/js/index/index.js
@@ -1,49 +1,44 @@
 ﻿
-$(document).ready(function(){
-	$.ajax({
-	    type : "post",
-		url : "sendEmail.html",
-		dataType:'json',
-	    data: {
-	    	
-	    },
-	    error : function(xhr) {
-			alert("系統繁忙，請稍後再試！");
-		},
-	    success : function(response) {
-	    	console.log(response);
-	    }
-	});
 
-	
-})
 
 
 
 
 function sendMailAjax(){
 	
-	$.ajax({
-		url : "sendEmailAjax99999.html",
-		type : "POST",
-//		dataType:'json',
-		data : {
-			//"logoDataObj":JSON.stringify(logoDataObj),
-		},
-		success : function(respone) {
-			console.log(respone);
-//			if(respone == "success"){
-//				//location.href='logo.html'; 
-//			} else {
-//				console.log(respone);
-//			}
-		},
-		error: function(xtl) {
-			console.log(xtl);
-			alert("系統繁忙，請稍後再試！");
+	
+	if($("#userNM").val() !="" && $("#userTEL").val() !="" && $("#userEM").val() !=""){
+		$(document).ready(function(){
+			$.ajax({
+			    type : "post",
+				url : "sendEmail.html",
+				dataType:'json',
+			    data: {
+			    	"applyEmailUserEmail": $("#userEM").val(),
+			    	"applyEmailUserName": $("#userNM").val(),
+			    	"applyEmailUserphone": $("#userTEL").val(),
+			    },
+			    error : function(xhr) {
+					alert("系統繁忙，請稍後再試！");
+				},
+			    success : function(response) {
+			    	alert("『PChome聯播網會有專人於2個工作天內與您聯繫！』");
+			    }
+			});
+		})
+	}else{
+		if($("#userNM").val() ==""){
+			alert("請填寫姓名!");	
 		}
-		}).done(function(msg) {
-			console.log(msg);
-		});
+		if($("#userTEL").val() ==""){
+			alert("請填寫電話!");	
+		}
+		if($("#userEM").val() ==""){
+			alert("請填寫Email!");	
+		}
+		
+	}
+	
+	
 }
 
